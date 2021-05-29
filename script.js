d2302a9265ee823ff328d23f805337af41230041
ericamonica <erikalaurindo07@gmail.com>
2021-05-29 09:34:16 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,21 @@
+//1 - Criar um loop que conte de 1 até 10 usando FOR
+for(let conta =1;conta <=10; conta ++){
+    console.log(conta)
+}
+
+
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+let i = 10
+while(i>=1){
+    console.log(i)
+    i--
+}
+
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+for(let numImpar=1; numImpar<=100;numImpar+=2){
+    console.log(numImpar)
+}
+//4- Criar um loop que conte todos os números pares de 0 a 100
+for(let numPar =0;numPar<=100;numPar+=2){
+    console.log(numPar)
+}
